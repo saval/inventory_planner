7e011fb7f3bee939e66e4a99cb3ac4428f8da68d
Alex <alex@freedcamp.com>
2021-03-18 14:17:24 +0200

    Process executor script changes to use separate configs with steps and data
    To parse potentially large file with steps JsonMachine library used

--- project/html/index.php
+++ project/html/index.php
@@ -10,34 +10,33 @@ error_reporting(E_ALL);
 require '../vendor/autoload.php';
 
 use Process\Storage\SimpleIStorage as Storage;
+use \JsonMachine\JsonMachine;
 
 if (empty($_GET['config'])) {
     die("Please specify 'config' parameter");
 }
 
-$config_file_path = '../config/' . $_GET['config'] . '.json';
-if (!file_exists($config_file_path)) {
-    die("Sorry, specified configuration file has not been found");
+$steps_config_file_path = '../config/' . $_GET['config'] . '.steps.json';
+$data_config_file_path = '../config/' . $_GET['config'] . '.data.json';
+if (!file_exists($steps_config_file_path) || !file_exists($data_config_file_path)) {
+    die("Sorry, specified configuration file(s) has not been found, please check");
 }
 
-$config_json = file_get_contents($config_file_path);
-if (false === $config_json) {
+$data_config_json = file_get_contents($data_config_file_path);
+if (false === $data_config_json) {
     die("Configuration file can not be read, please check file permissions");
 }
 
-$config_ar = json_decode($config_json, true);
-if (is_null($config_ar)) {
+$config_data = json_decode($data_config_json, true);
+if (is_null($config_data)) {
     die(json_last_error_msg());
 }
 
-if (empty($config_ar['steps'])) {
-    die('Sorry, no steps defined in the configuration file');
-}
-
-$storage = new Storage($config_ar['data'] ?? []);
+$storage = new Storage($config_data);
 
 $step = 1;
-foreach ($config_ar['steps'] as $step_data) {
+$config_steps = JsonMachine::fromFile($steps_config_file_path, "/steps");
+foreach ($config_steps as $step_data) {
     if (empty($step_data['command'])) {
         die("Process stopped as step #" . $step . ' does not have defined command');
     }
@@ -55,4 +54,6 @@ foreach ($config_ar['steps'] as $step_data) {
     } catch (Exception $e) {
         die("Process stopped as exception registered on step #" . $step . '. Message - ' . $e->getMessage());
     }
+    // based on https://stackoverflow.com/questions/10544779/how-can-i-clear-the-memory-while-running-a-long-php-script-tried-unset
+    $action = null;
 }
